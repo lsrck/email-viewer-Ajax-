b65f4d85556fec459ab2a46e34677b5880bd88cf
Louis Aurick <louisaurick@ntymail.com>
2018-03-01 18:57:29 +0100

    step3 done

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,7 @@
 class EmailsController < ApplicationController
 
-  before_action :all_emails, only: [:index, :create]
+  before_action :all_emails, only: [:index, :create, :update, :destroy]
+  before_action :set_emails, only: [:edit, :update, :destroy]
   respond_to :html, :js
 
   # index action has been removed
@@ -13,6 +14,16 @@ class EmailsController < ApplicationController
   	@email = Email.find(params[:id])
   end
 
+  def create
+    @email  = Email.create(task_params)
+  end
+
+
+  def destroy
+    @email.destroy
+  end
+
+
   private
 
     def all_emails
@@ -21,6 +32,10 @@ class EmailsController < ApplicationController
 
     def task_params
       params.require(:email).permit(:object, :body)
+    end
+
+    def set_emails
+      @email = Email.find(params[:id])
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ email1 = Email.create(object: "objet1", body: "body1")
+ email2 = Email.create(object: "objet2", body: "body2")
+ email3 = Email.create(object: "objet3", body: "body3")
+ email4 = Email.create(object: "objet4", body: "body4")
+ email5 = Email.create(object: "objet5", body: "body5")
