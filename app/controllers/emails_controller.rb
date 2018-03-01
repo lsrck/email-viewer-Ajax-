78f97556cbf9fdba9dab5fd27e3dd05ab17c9651
Louis Aurick <louisaurick@ntymail.com>
2018-03-01 16:50:53 +0100

    column + body(ajax)

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,26 @@
 class EmailsController < ApplicationController
-  def index
+
+  before_action :all_emails, only: [:index, :create]
+  respond_to :html, :js
+
+  # index action has been removed
+
+  def new
+    @email = Email.new
   end
+
+  def show
+  	@email = Email.find(params[:id])
+  end
+
+  private
+
+    def all_emails
+      @emails = Email.all
+    end
+
+    def task_params
+      params.require(:email).permit(:object, :body)
+    end
+
 end
