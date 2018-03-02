e1f5940578215da4dc524ac4bb676c2d479df3ef
Louis Aurick <louisaurick@ntymail.com>
2018-03-02 10:12:27 +0100

    ajout lien heroku readme

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301104209) do
+ActiveRecord::Schema.define(version: 20180301203415) do
 
   create_table "emails", force: :cascade do |t|
     t.string "object"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read"
   end
 
 end
